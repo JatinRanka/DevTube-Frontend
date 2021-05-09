ca4f79ebc26e4a746dc55b0dfd2aec70f6ca562f
jatin-ranka <jatin.ranka@freshworks.com>
2021-05-09 18:41:36 +0530

    Prettify fix

--- src/components/SaveVideoModal/index.jsx
+++ src/components/SaveVideoModal/index.jsx
@@ -1,78 +1,78 @@
-import React, { useState } from "react";
-import { isVideoInPlaylist } from "../../helper";
-import Modal from "../Modal";
+import React, { useState } from 'react';
+import { isVideoInPlaylist } from '../../helper';
+import Modal from '../Modal';
 import {
-  usePlaylistContext,
-  ADD_PLAYLIST,
-  ADD_VIDEO_IN_PLAYLIST,
-  REMOVE_VIDEO_FROM_PLAYLIST,
-} from "../../context/playlist";
-import "./index.scss";
+	usePlaylistContext,
+	ADD_PLAYLIST,
+	ADD_VIDEO_IN_PLAYLIST,
+	REMOVE_VIDEO_FROM_PLAYLIST
+} from '../../context/playlist';
+import './index.scss';
 
 const SavePlaylistModal = ({ showModal, handleCloseModal, videoDetails }) => {
-  const [newPlaylistName, setNewPlaylistName] = useState("");
+	const [newPlaylistName, setNewPlaylistName] = useState('');
 
-  const {
-    state: { playlists },
-    dispatch,
-  } = usePlaylistContext();
+	const {
+		state: { playlists },
+		dispatch
+	} = usePlaylistContext();
 
-  const handleAddPlaylistAndSave = () => {
-    if (!newPlaylistName) return;
+	const handleAddPlaylistAndSave = () => {
+		if (!newPlaylistName) return;
 
-    dispatch({
-      type: ADD_PLAYLIST,
-      payload: {
-        name: newPlaylistName,
-        listOfVideos: [{ ...videoDetails }],
-      },
-    });
-  };
+		dispatch({
+			type: ADD_PLAYLIST,
+			payload: {
+				name: newPlaylistName,
+				listOfVideos: [{ ...videoDetails }]
+			}
+		});
+	};
 
-  return (
-    <Modal showModal={showModal} handleCloseModal={handleCloseModal}>
-      <div className="video-page-modal">
-        <p>Save To</p>
-        <hr />
-        <div className="playlists">
-          {playlists.map((playlist) => {
-            return (
-              <div className="playlists-item" key={playlist._id}>
-                <input
-                  type="checkbox"
-                  className="checkbox"
-                  checked={isVideoInPlaylist({
-                    videoId: videoDetails._id,
-                    playlist,
-                  })}
-                  onClick={(event) => {
-                    dispatch({
-                      type: event.target.checked
-                        ? ADD_VIDEO_IN_PLAYLIST
-                        : REMOVE_VIDEO_FROM_PLAYLIST,
-                      payload: {
-                        video: { ...videoDetails },
-                        playlistId: playlist._id,
-                      },
-                    });
-                  }}
-                />
-                <label>{playlist.name}</label>
-              </div>
-            );
-          })}
-        </div>
-        <hr />
+	return (
+		<Modal showModal={showModal} handleCloseModal={handleCloseModal}>
+			<div className="video-page-modal">
+				<p>Save To</p>
+				<hr />
+				<div className="playlists">
+					{playlists.map((playlist) => {
+						return (
+							<div className="playlists-item" key={playlist._id}>
+								<input
+									type="checkbox"
+									className="checkbox"
+									checked={isVideoInPlaylist({
+										videoId: videoDetails._id,
+										playlist
+									})}
+									onClick={(event) => {
+										dispatch({
+											type: event.target.checked
+												? ADD_VIDEO_IN_PLAYLIST
+												: REMOVE_VIDEO_FROM_PLAYLIST,
+											payload: {
+												video: { ...videoDetails },
+												playlistId: playlist._id
+											}
+										});
+									}}
+								/>
+								<label>{playlist.name}</label>
+							</div>
+						);
+					})}
+				</div>
+				<hr />
 
-        <input
-          placeholder="Add playlist name"
-          value={newPlaylistName}
-          onChange={(event) => setNewPlaylistName(event.target.value)}
-        />
-        <button onClick={handleAddPlaylistAndSave}>Save</button>
-      </div>
-    </Modal>
-  );
+				<input
+					placeholder="Add playlist name"
+					value={newPlaylistName}
+					onChange={(event) => setNewPlaylistName(event.target.value)}
+				/>
+				<button onClick={handleAddPlaylistAndSave}>Save</button>
+			</div>
+		</Modal>
+	);
 };
 
 export default SavePlaylistModal;
--- src/views/PLayListsView/index.jsx
+++ src/views/PLayListsView/index.jsx
@@ -1,35 +1,35 @@
-import React from "react";
-import { useParams } from "react-router";
-import VideoPageComponent from "../../components/VideoPageComponent";
-import { usePlaylistContext } from "../../context/playlist";
-import empytPlaylistImage from "../../assets/images/empty-playlist.svg";
-import "./index.scss";
+import React from 'react';
+import { useParams } from 'react-router';
+import VideoPageComponent from '../../components/VideoPageComponent';
+import { usePlaylistContext } from '../../context/playlist';
+import empytPlaylistImage from '../../assets/images/empty-playlist.svg';
+import './index.scss';
 
 const renderEmptyPlaylistComponent = () => {
-  return (
-    <div className="empty-playlist-container">
-      <img src={empytPlaylistImage} className="empty-playlist-img" />
-      <p className="heading-4">Playlist is Empty.</p>
-    </div>
-  );
+	return (
+		<div className="empty-playlist-container">
+			<img src={empytPlaylistImage} className="empty-playlist-img" />
+			<p className="heading-4">Playlist is Empty.</p>
+		</div>
+	);
 };
 
 const PlayListsView = () => {
-  const { playlistId } = useParams();
-  const {
-    state: { playlists },
-  } = usePlaylistContext();
+	const { playlistId } = useParams();
+	const {
+		state: { playlists }
+	} = usePlaylistContext();
 
-  const playlist = playlists.find((playlist) => playlist._id === playlistId);
+	const playlist = playlists.find((playlist) => playlist._id === playlistId);
 
-  return playlist.listOfVideos.length === 0 ? (
-    renderEmptyPlaylistComponent()
-  ) : (
-    <VideoPageComponent
-      videoDetails={playlist.listOfVideos[0]}
-      videosList={playlist.listOfVideos}
-    />
-  );
+	return playlist.listOfVideos.length === 0 ? (
+		renderEmptyPlaylistComponent()
+	) : (
+		<VideoPageComponent
+			videoDetails={playlist.listOfVideos[0]}
+			videosList={playlist.listOfVideos}
+		/>
+	);
 };
 
 export default PlayListsView;
